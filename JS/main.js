5ff2cb2a711165e40b349a65a622050f8fe18005
RodrigoBentinNai <rodrigo.bentin.rbn@gmail.com>
2023-01-23 20:32:47 -0300

    2da preentrega

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,35 @@
+//RODRIGO MARIO BENTIN NAI
+//"APP DE STOCK PARA MATERIALES DE CONSTRUCCION"
+
 /*
-RODRIGO MARIO BENTIN NAI
-APLICACION DE STOCK PARA MATERIALES DE CONSTRUCCION
-1 solo material en este caso
+let material = 0;
+
+case "1":
+
+alert("Stock actual: " + material + " Unidades");
+break;
+
+case "2":
+    let ingreso = parseInt(prompt("Cantidad de Unidades a ingresar"));
+    material += ingreso;
+    alert("Stock actualizado: " + material + " Unidades");
+break;
+
+case "3":
+    let egreso = parseInt(prompt("Cantidad de Unidades a despachar"));
+    if (egreso <= material) {
+        material = material - egreso;
+        alert("Stock actualizado: " + material + " Unidades");
+    } else {
+        alert("Stock insuficiente \nVerifica Stock disponible e intenta nuevamente")
+    }
+break;
 */
 
 let userName = "rodrigo"
 let password = "1357";
 
-//funcion
+
 function login() {
 
     let access = false;
@@ -27,40 +49,93 @@ function login() {
     return access
 }
 
-//condición
-if (login()) {
-    let cemento = 10000;
+class Materiales {
+    constructor(id, nombre, cantidad) {
+    this.id = Number(id);
+    this.nombre = nombre;
+    this.cantidad = Number(cantidad);
+    }
+}
 
-    let option = prompt("Elegir opción: \n1 - Stock de Cemento \n2 - Ingreso \n3 - Egreso \n4 - Salir");
-    //ciclo
-    while (option != "4"){
-        //opciones del stock
+const stockMateriales = [];
+
+function crearNuevoMaterial() {
+    let material = new Materiales(
+        prompt("ingrese codigo del material"),
+        prompt("ingrese nombre del material"),
+        prompt("ingrese cantidad de unidades")
+    );
+    stockMateriales.push(material);
+}
+    
+function listadoDeMateriales() {
+    if (stockMateriales.length == 0) {
+        alert("Material Inexistente");
+        } else {
+    for (const material of stockMateriales) {
+        alert(
+            `id: ${material.id}\n nombre: ${material.nombre} \n unidades: ${material.cantidad}`
+            );
+        }
+    }
+}
+    
+function modificarMaterial() {
+    if (stockMateriales.length == 0) {
+        alert("Material Inexistente");
+    } else {
+        let modificar = prompt("Ingrese Id de Material a modificar");
+
+        for (const material of stockMateriales) {
+            if (material.id === Number(modificar)) {
+                material.nombre = prompt("Ingrese Nombre Nuevo")
+                material.cantidad = Number(prompt("Ingrese nueva Cantidad"));
+
+                console.log(stockMateriales);
+            }
+        }
+    }
+}
+
+let stockMaterial = []
+
+function ingresoCantidades() {
+
+    let elejirMaterial = parseInt(prompt("ingrese ID del Material")); 
+
+    let agregarCantidad = stockMaterial.find((el) => el.id == elejirMaterial)
+    stockMaterial.push(agregarCantidad);
+    
+    alert(stockMaterial);
+}
+
+if (login()) {    
+    
+    let option = prompt("Elegir opción: \n1 - Materiales \n2 - Nuevo Material \n3 - Modificar Material \n4 - Agregar Stock \n5 - Salir");
+
+    while (option != "5") {
         switch (option) {
             case "1":
-                alert("Stock actual: " + cemento + " Bolsas de Cemento x 50Kg");
+                listadoDeMateriales();
             break;
             
             case "2":
-                let ingreso = parseInt(prompt("Cantidad de Bolsas a ingresar"));
-                cemento += ingreso;
-                alert("Stock actualizado: " + cemento + " Bolsas de Cemento x 50Kg");
+                crearNuevoMaterial();
             break;
 
             case "3":
-                let egreso = parseInt(prompt("Cantidad de bolsas a despachar"));
-                if (egreso <= cemento) {
-                    cemento = cemento - egreso;
-                    alert("Stock actualizado: " + cemento + " Bolsas de Cemento x 50Kg");
-                } else {
-                    alert("Stock insuficiente \nVerifica Stock disponible e intenta nuevamente")
-                }
+                modificarMaterial();
+            break;
+
+            case "4":
+                ingresoCantidades();
             break;
 
             default:
                 alert("Opción elegida inválida");
             break;
         }
-        option = prompt("Elegir opción: \n1 - Stock de Cemento \n2 - Ingreso \n3 - Egreso \n4 - Salir");
+        option = prompt("Elegir opción: \n1 - Materiales \n2 - Nuevo Material \n3 - Modificar Material \n4 - Agregar Stock \n5 - Salir");
     }
 
 } else {
